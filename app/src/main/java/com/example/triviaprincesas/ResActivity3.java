04a8283f58466f0be4606e89020f548bdb82652b
Armando Hernandez <armando_1319104150@uptecamac.edu.mx>
2022-03-11 19:45:02 -0600

    prodiedad para poner la imagen el el ImageView borrado y mostrar toast

--- app/src/main/java/com/example/triviaprincesas/ResActivity3.java
+++ app/src/main/java/com/example/triviaprincesas/ResActivity3.java
@@ -31,31 +31,31 @@ public class ResActivity3 extends AppCompatActivity {
 
             fotoPrinces.setImageResource(R.drawable.cenicienta);
             descripcion.setText(desc);
-            Toast.makeText(this, "Cenicienta", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Cenicienta", Toast.LENGTH_SHORT).show();
         }else if(puntuacion >= 25 && puntuacion < 37.5){
             //aurora
             fotoPrinces.setImageResource(R.drawable.aurora);
-            Toast.makeText(this, "Aurora", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Aurora", Toast.LENGTH_SHORT).show();
         }else if(puntuacion >= 37.5 && puntuacion < 50){
             //Ariel
             fotoPrinces.setImageResource(R.drawable.ariel);
-            Toast.makeText(this, "Ariel", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Ariel", Toast.LENGTH_SHORT).show();
         }else if(puntuacion >= 50 && puntuacion < 67.5){
             //rapuncel
             fotoPrinces.setImageResource(R.drawable.rapunzel);
-            Toast.makeText(this, "Rapuncel", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Rapuncel", Toast.LENGTH_SHORT).show();
         }else if(puntuacion >= 67.5 && puntuacion < 75){
             //elsa
             fotoPrinces.setImageResource(R.drawable.elsa);
-            Toast.makeText(this, "Elsa", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Elsa", Toast.LENGTH_SHORT).show();
         }else if(puntuacion >=75 && puntuacion < 87.5){
             //Mulan
             fotoPrinces.setImageResource(R.drawable.mulan);
-            Toast.makeText(this, "Mulan", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Mulan", Toast.LENGTH_SHORT).show();
         }else if(puntuacion >= 87.5 && puntuacion <= 100){
             //bella
             fotoPrinces.setImageResource(R.drawable.bella);
-            Toast.makeText(this, "Bella", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Bella", Toast.LENGTH_SHORT).show();
         }
 
         btnInicio.setOnClickListener(new View.OnClickListener() {
